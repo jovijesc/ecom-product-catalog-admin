6740188155f0ee4f59c4a7225c89a3a4fa3acdfb
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2023-05-12 17:34:21 -0300

    Implementing the base classes for DDD

---
+++ domain/src/main/java/AggregateRoot.java
@@ -0,0 +1,5 @@
+public abstract class AggregateRoot<ID extends Identifier> extends Entity<ID> {
+    public AggregateRoot(ID id) {
+        super(id);
+    }
+}
---
+++ domain/src/main/java/com/ecom/catalog/admin/domain/product/Product.java
@@ -0,0 +1,4 @@
+package com.ecom.catalog.admin.domain.product;
+
+public class Product {
+}
