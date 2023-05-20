c9bba8bfae4cc9681128fbb00df6eccbed86e3fd
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2023-05-20 10:32:23 -0300

    Implementing integrated tests for the use case of category creation

--- infrastructure/src/test/java/com/ecom/catalog/admin/IntegrationTest.java
+++ infrastructure/src/test/java/com/ecom/catalog/admin/IntegrationTest.java
@@ -11,7 +11,7 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
-@ActiveProfiles("test-integration")
+@ActiveProfiles("test")
 @SpringBootTest(classes = WebServerConfig.class)
 @ExtendWith(MySQLCleanUpExtension.class)
 @Tag("integrationTest")
---
+++ infrastructure/src/test/java/com/ecom/catalog/admin/application/category/create/CreateCategoryUseCaseIT.java
@@ -0,0 +1,89 @@
+package com.ecom.catalog.admin.application.category.create;
+
+import com.ecom.catalog.admin.IntegrationTest;
+import com.ecom.catalog.admin.domain.category.CategoryGateway;
+import com.ecom.catalog.admin.domain.exceptions.NotificationException;
+import com.ecom.catalog.admin.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.util.Objects;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@IntegrationTest
+public class CreateCategoryUseCaseIT {
+
+    @Autowired
+    private DefaultCreateCategoryUseCase useCase;
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
+        // given
+        final var expectedName = "Eletronicos";
+        final var expectedDescription = "Eletronicos do tipo abc";
+        final var expectedIsActive = true;
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        // when
+        final var actualOutput = useCase.execute(aCommand);
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        final var actualCategory = categoryRepository.findById(actualOutput.id()).get();
+
+        // then
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+
+    }
+
+    @Test
+    public void givenAnInvalidNullName_whenCallsCreateCategory_shouldThrowsNotificationException() {
+        // given
+        final String expectedName = null;
+        final var expectedDescription = "Eletronicos do tipo abc";
+        final var expectedIsActive = true;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        // when
+        final var actualException = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+
+        // then
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        Mockito.verify(categoryGateway, times(0)).create(any());
+    }
+
+}
