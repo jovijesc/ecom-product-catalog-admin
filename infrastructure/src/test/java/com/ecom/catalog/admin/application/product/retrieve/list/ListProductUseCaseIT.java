2fa8f420f1c7ec69ed323ddf3729354e58c82704
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2023-05-22 11:29:20 -0300

    Implementing integrated tests for the use case of product findall

--- infrastructure/src/test/java/com/ecom/catalog/admin/application/product/retrieve/list/ListProductUseCaseIT.java
+++ infrastructure/src/test/java/com/ecom/catalog/admin/application/product/retrieve/list/ListProductUseCaseIT.java
@@ -1,10 +1,8 @@
 package com.ecom.catalog.admin.application.product.retrieve.list;
 
-import com.ecom.catalog.admin.application.product.UseCaseTest;
-import com.ecom.catalog.admin.application.product.retrieve.list.DefaultListProductUseCase;
-import com.ecom.catalog.admin.application.product.retrieve.list.ProductListOutput;
-import com.ecom.catalog.admin.domain.category.CategoryID;
-import com.ecom.catalog.admin.domain.pagination.Pagination;
+import com.ecom.catalog.admin.IntegrationTest;
+import com.ecom.catalog.admin.domain.category.Category;
+import com.ecom.catalog.admin.domain.category.CategoryGateway;
 import com.ecom.catalog.admin.domain.pagination.SearchQuery;
 import com.ecom.catalog.admin.domain.product.Money;
 import com.ecom.catalog.admin.domain.product.Product;
@@ -12,44 +10,46 @@ import com.ecom.catalog.admin.domain.product.ProductGateway;
 import com.ecom.catalog.admin.domain.product.ProductStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
 
+@IntegrationTest
+public class ListProductUseCaseIT {
 
-class ListProductUseCaseTest extends UseCaseTest {
-
-    @InjectMocks
+    @Autowired
     private DefaultListProductUseCase useCase;
 
-    @Mock
+    @SpyBean
     private ProductGateway productGateway;
 
-    @Override
-    protected List<Object> getMocks() {
-        return List.of(productGateway);
-    }
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
 
     @Test
     public void givenAValidQuery_whenCallsListProduct_shouldReturnProducts() {
         // given
+        final var expectedCategoryOne =
+                categoryGateway.create(Category.newCategory("Eletrônico", "Eletrônicos do tipo A", true));
+        final var expectedCategoryTwo =
+                categoryGateway.create(Category.newCategory("Informática", "Informática do tipo A", true));
+
         final var products = List.of(
-                Product.newProduct("Celular", "Celular do tipo ABC", ProductStatus.ACTIVE, Money.with(3000.0), 10, CategoryID.from("123")),
-                Product.newProduct("Notebook", "Notebook do tipo 123", ProductStatus.ACTIVE, Money.with(5000.0), 10, CategoryID.from("456"))
+                productGateway.create(Product.newProduct("Celular", "Celular do tipo ABC", ProductStatus.ACTIVE, Money.with(3000.0), 10, expectedCategoryOne.getId())),
+                productGateway.create(Product.newProduct("Notebook", "Notebook do tipo 123", ProductStatus.ACTIVE, Money.with(5000.0), 10, expectedCategoryTwo.getId()))
         );
 
         final var expectedPage = 0;
         final var expectedPerPage = 10;
-        final var expectedTerms = "A";
+        final var expectedTerms = "e";
         final var expectedSort = "createdAt";
         final var expectedDirection = "asc";
         final var expectedTotal = 2;
@@ -57,16 +57,6 @@ class ListProductUseCaseTest extends UseCaseTest {
         final var expectedItems = products.stream()
                 .map(ProductListOutput::from)
                 .toList();
-
-        final var expectedPagination = new Pagination<>(
-            expectedPage,
-            expectedPerPage,
-            expectedTotal,
-            products
-        );
-
-        when(productGateway.findAll(any()))
-                .thenReturn(expectedPagination);
 
         final var aQuery =
                 new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
@@ -79,15 +69,16 @@ class ListProductUseCaseTest extends UseCaseTest {
         Assertions.assertEquals(expectedPerPage, actualOutput.perPage());
         Assertions.assertEquals(expectedTotal, actualOutput.total());
         Assertions.assertEquals(expectedItems, actualOutput.items());
-
-        Mockito.verify(productGateway, times(1)).findAll(eq(aQuery));
+        Assertions.assertTrue(
+                expectedItems.size() == actualOutput.items().size()
+                        && expectedItems.containsAll(actualOutput.items())
+        );
 
     }
 
     @Test
     public void givenAValidQuery_whenCallsListProductAndResultIsEmpty_shouldReturnProducts() {
         // given
-        final var products = List.<Product>of();
 
         final var expectedPage = 0;
         final var expectedPerPage = 10;
@@ -98,16 +89,6 @@ class ListProductUseCaseTest extends UseCaseTest {
 
         final var expectedItems = List.<ProductListOutput>of();
 
-        final var expectedPagination = new Pagination<>(
-                expectedPage,
-                expectedPerPage,
-                expectedTotal,
-                products
-        );
-
-        when(productGateway.findAll(any()))
-                .thenReturn(expectedPagination);
-
         final var aQuery =
                 new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
 
@@ -119,8 +100,6 @@ class ListProductUseCaseTest extends UseCaseTest {
         Assertions.assertEquals(expectedPerPage, actualOutput.perPage());
         Assertions.assertEquals(expectedTotal, actualOutput.total());
         Assertions.assertEquals(expectedItems, actualOutput.items());
-
-        Mockito.verify(productGateway, times(1)).findAll(eq(aQuery));
 
     }
 
@@ -135,8 +114,8 @@ class ListProductUseCaseTest extends UseCaseTest {
 
         final var expectedErrorMessage = "Gateway error";
 
-        when(productGateway.findAll(any()))
-                .thenThrow(new IllegalStateException(expectedErrorMessage));
+        doThrow(new IllegalStateException(expectedErrorMessage))
+                .when(productGateway).findAll(any());
 
         final var aQuery =
                 new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
