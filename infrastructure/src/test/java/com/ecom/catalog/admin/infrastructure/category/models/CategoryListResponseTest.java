5191069b602877345dae6d9064240a4d05987286
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2023-05-24 14:46:34 -0300

    Implementing deserialization tests of the CategoryListResponse class

---
+++ infrastructure/src/test/java/com/ecom/catalog/admin/infrastructure/category/models/CategoryListResponseTest.java
@@ -0,0 +1,47 @@
+package com.ecom.catalog.admin.infrastructure.category.models;
+
+import com.ecom.catalog.admin.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.time.Instant;
+
+@JacksonTest
+public class CategoryListResponseTest {
+
+    @Autowired
+    private JacksonTester<CategoryListResponse> json;
+
+    @Test
+    public void testMarshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = "Eletronicos";
+        final var expectedDescription = "Eletronicos do tipo abc";
+        final var expectedIsActive = true;
+        final var expectedCreatedAt = Instant.now();
+        final var expectedDeletedAt = Instant.now();
+
+        final var response = new CategoryListResponse(
+                expectedId,
+                expectedName,
+                expectedDescription,
+                expectedIsActive,
+                expectedCreatedAt,
+                expectedDeletedAt
+        );
+
+        final var actualJson = this.json.write(response);
+
+        Assertions.assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.description", expectedDescription)
+                .hasJsonPathValue("$.is_active", expectedIsActive)
+                .hasJsonPathValue("$.created_at", expectedCreatedAt.toString())
+                .hasJsonPathValue("$.deleted_at", expectedDeletedAt.toString());
+    }
+
+
+}
